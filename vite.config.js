b8bebf9c7f4b8ee39b22f665b6e4247270f8ec95
Adria Blancafort <adriablancafort@gmail.com>
2024-10-11 20:44:57 +0200

    feat: add shadcn-svelte

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from "path";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib"),
+    },
+  },
 });
