33a5f57ef29a349e23a9932f8f0d98e4205357f4
Adria Blancafort <adriablancafort@gmail.com>
2024-10-13 11:19:44 +0200

    refactor: remove user store to simplify

--- src/routes/(authorised)/+layout.js
+++ src/routes/(authorised)/+layout.js
@@ -1,10 +1,9 @@
-import { user } from "$lib/stores/users";
-import { get } from "svelte/store";
+import { pb } from "$lib/pocketbase.js";
 import { redirect } from "@sveltejs/kit";
 
 export async function load() {
   // User is not authenticated, login required
-  if (!get(user)) {
+  if (!pb.authStore.isValid) {
     throw redirect(302, "/inicia-sessio");
   }
 
--- src/routes/(unauthorised)/+layout.js
+++ src/routes/(unauthorised)/+layout.js
@@ -1,10 +1,9 @@
-import { user } from "$lib/stores/users";
-import { get } from "svelte/store";
+import { pb } from "$lib/pocketbase.js";
 import { redirect } from "@sveltejs/kit";
 
 export async function load() {
   // User is authenticated, skip login
-  if (get(user)) {
+  if (pb.authStore.isValid) {
     throw redirect(302, "/");
   }
 
