313eaebe5e31c942de2b19156a45573fcbfbf66f
Adria Blancafort <adriablancafort@gmail.com>
2024-10-13 22:54:06 +0200

    remove unused params

--- src/routes/(authorised)/apunts/+page.js
+++ src/routes/(authorised)/apunts/+page.js
@@ -1,6 +1,6 @@
 import { pb } from "$lib/pocketbase.js";
 
-export async function load({ params }) {
+export async function load() {
   const apunts = await pb.collection("apunts").getFullList();
   return {
     apunts: apunts,
