51bf748dcba92ff17df0d5781a06d2471fbd736d
Adria Blancafort <adriablancafort@gmail.com>
2024-10-13 02:33:19 +0200

    feat: add penja apunts logic

---
+++ src/routes/(authorised)/penja-apunts/+page.svelte
@@ -0,0 +1,59 @@
+<script>
+  import { Button } from "$lib/components/ui/button/index.js";
+  import * as Card from "$lib/components/ui/card/index.js";
+  import { Input } from "$lib/components/ui/input/index.js";
+  import { Label } from "$lib/components/ui/label/index.js";
+  import { pb } from "$lib/pocketbase.js";
+  import { LoaderCircle } from "lucide-svelte";
+  
+  let title;
+  let files;
+  let formLoading = false;
+
+  async function upload() {
+    formLoading = true;
+    try {
+      const data = new FormData();
+      data.append("title", title);
+      data.append("file", files[0]);
+      const record = await pb.collection("apunts").create(data);
+      // success
+    } catch (error) {
+      // error
+    } finally {
+      formLoading = false;
+    }
+  }
+</script>
+
+<svelte:head>
+  <title>Penja Apunts - Apunts Dades</title>
+</svelte:head>
+
+<div class="flex items-center justify-center h-screen">
+  <Card.Root class="max-w-sm mx-auto">
+    <Card.Header>
+      <Card.Title class="text-2xl">Penja Apunts</Card.Title>
+      <Card.Description>Penja apunts a Apunts Dades.</Card.Description>
+    </Card.Header>
+    <Card.Content>
+      <form class="grid gap-4" on:submit={upload}>
+        <div class="grid gap-2">
+          <Label for="title">Títol</Label>
+          <Input id="title" type="text" bind:value={title} required />
+        </div>
+        <div class="grid w-full max-w-sm items-center gap-1.5">
+          <Label for="file">Arxiu</Label>
+          <input type="file" bind:files required>
+        </div>
+        <Button type="submit" class="w-full" disabled={formLoading}>
+          {#if formLoading}
+            <LoaderCircle class="h-5 animate-spin" />
+          {:else}
+            Penja Apunts
+          {/if}
+        </Button>
+      </form>
+    </Card.Content>
+  </Card.Root>
+</div>
