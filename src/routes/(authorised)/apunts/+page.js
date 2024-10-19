db5f3c7c31a9085ba8b95619965aa0cdc9955689
Adria Blancafort <adriablancafort@gmail.com>
2024-10-19 19:35:17 +0200

    fix: avoid database query without auth check

--- src/routes/(authorised)/apunts/+page.js
+++ src/routes/(authorised)/apunts/+page.js
@@ -1,6 +1,7 @@
 import { pb } from "$lib/pocketbase.js";
 
-export async function load() {
+export async function load({ parent }) {
+  await parent();
   const apunts = await pb.collection("apunts").getFullList();
   return {
     apunts: apunts,
