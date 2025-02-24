40f7069ebdd9c047ef1153529da2927f4615869e
Adrià Blancafort <adriablancafort@gmail.com>
2025-02-24 23:04:58 +0100

    remove old pages

--- src/routes/(authorised)/apunts/+page.ts
+++
@@ -1,9 +0,0 @@
-import { pb } from "$lib/pocketbase.ts";
-
-export async function load({ parent }) {
-  await parent();
-  const apunts = await pb.collection("apunts").getFullList();
-  return {
-    apunts: apunts,
-  };
-}
