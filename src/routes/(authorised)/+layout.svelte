f0b7586a724e19724755bd0b5f04ec71fbb0ca17
Adrià Blancafort <adriablancafort@gmail.com>
2025-02-24 23:03:06 +0100

    sidebar layout

---
+++ src/routes/(authorised)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import AppSidebar from "$lib/components/app-sidebar.svelte";
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+
+	let { children } = $props();
+</script>
+
+<Sidebar.Provider>
+	<AppSidebar />
+	<Sidebar.Inset>
+    {@render children()}
+	</Sidebar.Inset>
+</Sidebar.Provider>
