8d781650a38b84830a79698a40dc6d54648957ec
Adrià Blancafort <adriablancafort@gmail.com>
2025-02-26 23:26:55 +0100

    external link icon

--- src/lib/components/NoteCard.svelte
+++ src/lib/components/NoteCard.svelte
@@ -2,13 +2,15 @@
     import * as Card from "$lib/components/ui/card/index.js";
     import { Badge } from "$lib/components/ui/badge/index.js";
     import { getFileUrl } from "$lib/pocketbase";
+    import { ExternalLink } from "lucide-svelte";
 
     let { note } = $props();
 </script>
 
 <a href={getFileUrl(note)} target="_blank">
-    <Card.Root class="relative pb-6">
-        <Badge variant="outline" class="absolute top-4 right-4">{note.type}</Badge>
+    <Card.Root class="relative pb-6 hover:bg-muted/50 transition-colors">
+        <Badge variant="outline" class="absolute bottom-6 right-4">{note.type}</Badge>
+        <ExternalLink class="absolute top-4 right-4 h-5" />
         <Card.Header>
             <Card.Title>{note.title}</Card.Title>
             <Card.Description>{note.title}</Card.Description>
