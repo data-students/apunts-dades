72fe2307af8d50180498254f638904b27f29b0e9
Adrià Blancafort <adriablancafort@gmail.com>
2025-02-26 20:03:57 +0100

    feat: add classify apunts tab

--- src/routes/(authorised)/assignatura/[subject_id]/+page.svelte
+++ src/routes/(authorised)/assignatura/[subject_id]/+page.svelte
@@ -1,38 +1,57 @@
 <script lang="ts">
+    import * as Tabs from "$lib/components/ui/tabs/index.js";
+    import { Badge } from "$lib/components/ui/badge/index.js";
     import { getFileUrl } from "$lib/pocketbase";
 
 	let { data } = $props();
+    let selectedTab = $state("all");
+
+    function getNotesByType(notes, type: string) {
+        if (type === "all") return notes;
+        return notes.filter((note) => note.type === type);
+    }
 </script>
 
-<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
-    {#await data.subject}
-        Loading subject...
-    {:then subject}
+
+{#await data.subject}
+    Loading subject...
+{:then subject}
+    <div>
         <h2 class="text-3xl font-semibold">{subject.title}</h2>
-        <p>Q{subject.quarter}</p>
-    {:catch error}
-        <p>Error loading subject: {error.message}</p>
-    {/await}
+        <Badge variant="outline">{subject.quarter}</Badge>
+    </div>
+{:catch error}
+    <p>Error loading subject: {error.message}</p>
+{/await}
 
-    {#await data.notes}
-        Loading notes...
-    {:then notes}
-        {#each data.notes as notes}
-            <a href={getFileUrl(notes)} target="_blank">
-                <div class="space-y-2 hover:opacity-80 transition-opacity">
-                    <h2>{notes.title}</h2>
-                </div>
-            </a>
-        {/each}
-    {:catch error}
-        <p>Error loading notes: {error.message}</p>
-    {/await}
+<Tabs.Root value={selectedTab} class="w-full" onValueChange={(value) => selectedTab = value}>
+    <Tabs.List>
+        <Tabs.Trigger value="all">Tots</Tabs.Trigger>
+        <Tabs.Trigger value="theory">Teoria</Tabs.Trigger>
+        <Tabs.Trigger value="lab">Laboratori</Tabs.Trigger>
+        <Tabs.Trigger value="exam">Examens</Tabs.Trigger>
+    </Tabs.List>
+</Tabs.Root>
 
-	<div class="grid auto-rows-min gap-4 md:grid-cols-3">
-		<div class="bg-muted/50 aspect-video rounded-xl"></div>
-		<div class="bg-muted/50 aspect-video rounded-xl"></div>
-		<div class="bg-muted/50 aspect-video rounded-xl"></div>
-	</div>
+{#await data.notes}
+    Loading notes...
+{:then notes}
+    {#each getNotesByType(notes, selectedTab) as notes}
+        <a href={getFileUrl(notes)} target="_blank">
+            <div class="space-y-2 hover:opacity-80 transition-opacity">
+                <h2>{notes.title}</h2>
+            </div>
+        </a>
+    {/each}
+{:catch error}
+    <p>Error loading notes: {error.message}</p>
+{/await}
 
-	<div class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min"></div>
+<div class="grid auto-rows-min gap-4 md:grid-cols-3">
+    <div class="bg-muted/50 aspect-video rounded-xl"></div>
+    <div class="bg-muted/50 aspect-video rounded-xl"></div>
+    <div class="bg-muted/50 aspect-video rounded-xl"></div>
 </div>
+
+<div class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min"></div>
+
