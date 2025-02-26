e474a09104bf7bf4047b512964c9c177b63a0518
Adrià Blancafort <adriablancafort@gmail.com>
2025-02-27 00:41:49 +0100

    display user name, email and avatar

--- src/lib/components/nav-user.svelte
+++ src/lib/components/nav-user.svelte
@@ -24,7 +24,9 @@
 	const sidebar = useSidebar();
 
 	import { goto } from "$app/navigation";
-  	import { pb } from "$lib/pocketbase.ts";
+    import { pb, getUserAvatarUrl } from "$lib/pocketbase.ts";
+
+	const currentUser = pb.authStore.model;
 
 	async function logout() {
 		pb.authStore.clear();
@@ -43,12 +45,12 @@
 						class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
 					>
 						<Avatar.Root class="h-8 w-8 rounded-lg">
-							<Avatar.Image src={user.avatar} alt={user.name} />
-							<Avatar.Fallback class="rounded-lg">CN</Avatar.Fallback>
+							<Avatar.Image class="rounded-full" src={getUserAvatarUrl(currentUser)} alt={user.name} />
+							<Avatar.Fallback class="rounded-full">Al</Avatar.Fallback>
 						</Avatar.Root>
 						<div class="grid flex-1 text-left text-sm leading-tight">
-							<span class="truncate font-semibold">{user.name}</span>
-							<span class="truncate text-xs">{user.email}</span>
+							<span class="truncate font-semibold">{currentUser.name}</span>
+							<span class="truncate text-xs">{currentUser.email}</span>
 						</div>
 						<ChevronsUpDown class="ml-auto size-4" />
 					</Sidebar.MenuButton>
@@ -63,12 +65,12 @@
 				<DropdownMenu.Label class="p-0 font-normal">
 					<div class="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
 						<Avatar.Root class="h-8 w-8 rounded-lg">
-							<Avatar.Image src={user.avatar} alt={user.name} />
-							<Avatar.Fallback class="rounded-lg">CN</Avatar.Fallback>
+							<Avatar.Image class="rounded-full" src={getUserAvatarUrl(currentUser)} alt={user.name} />
+							<Avatar.Fallback class="rounded-full">Al</Avatar.Fallback>
 						</Avatar.Root>
 						<div class="grid flex-1 text-left text-sm leading-tight">
-							<span class="truncate font-semibold">{user.name}</span>
-							<span class="truncate text-xs">{user.email}</span>
+							<span class="truncate font-semibold">{currentUser.name}</span>
+							<span class="truncate text-xs">{currentUser.email}</span>
 						</div>
 					</div>
 				</DropdownMenu.Label>
