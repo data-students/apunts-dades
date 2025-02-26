c21f8fb5615834bde3ae9c44bc908e060f388df3
Adrià Blancafort <adriablancafort@gmail.com>
2025-02-26 20:07:04 +0100

    add badge

--- src/routes/(authorised)/+page.svelte
+++ src/routes/(authorised)/+page.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
+	import { Badge } from "$lib/components/ui/badge/index.js";
+
 	let { data } = $props();
 </script>
 
-<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
-	{#await data.subjects}
-		Loading subjects...
-	{:then subjects}
-		{#each data.subjects as subject}
-			<a href={`/assignatura/${subject.id}`}>
-				<div class="space-y-2 hover:opacity-80 transition-opacity">
-					<h2>{subject.title}</h2>
-					<p>Q{subject.quarter}</p>
-				</div>
-			</a>
-		{/each}
-	{:catch error}
-		<p>Error loading subjects: {error.message}</p>
-	{/await}
+{#await data.subjects}
+	Loading subjects...
+{:then subjects}
+	{#each subjects as subject}
+		<a href={`/assignatura/${subject.id}`}>
+			<div class="space-y-2 hover:opacity-80 transition-opacity">
+				<h2>{subject.title}</h2>
+				<Badge variant="outline">{subject.quarter}</Badge>
+			</div>
+		</a>
+	{/each}
+{:catch error}
+	<p>Error loading subjects: {error.message}</p>
+{/await}
 
-	<div class="grid auto-rows-min gap-4 md:grid-cols-3">
-		<div class="bg-muted/50 aspect-video rounded-xl"></div>
-		<div class="bg-muted/50 aspect-video rounded-xl"></div>
-		<div class="bg-muted/50 aspect-video rounded-xl"></div>
-	</div>
-
-	<div class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min"></div>
+<div class="grid auto-rows-min gap-4 md:grid-cols-3">
+	<div class="bg-muted/50 aspect-video rounded-xl"></div>
+	<div class="bg-muted/50 aspect-video rounded-xl"></div>
+	<div class="bg-muted/50 aspect-video rounded-xl"></div>
 </div>
+
+<div class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min"></div>
