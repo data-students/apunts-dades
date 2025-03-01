6407947ae831d12e5cf27be0333112ffe34a8dae
Adrià Blancafort <adriablancafort@gmail.com>
2025-03-01 14:48:22 +0100

    switch to table on edit user uploaded notes page

--- src/routes/(authorised)/usuari/apunts-penjats/+page.svelte
+++ src/routes/(authorised)/usuari/apunts-penjats/+page.svelte
@@ -1,23 +1,56 @@
 <script lang="ts">
-    import UserNoteCard from "$lib/components/UserNoteCard.svelte";
-    import Button from "$lib/components/ui/button/button.svelte";
+    import * as Table from "$lib/components/ui/table/index.js";
+    import { Button } from "$lib/components/ui/button";
     import Upload from "lucide-svelte/icons/upload";
+    import Edit from "lucide-svelte/icons/pencil";
 
     let { data } = $props();
 
     let userNotes = data.notes.filter(note => note.author === data.user.id);
+
+    function formatDate(dateString) {
+        const date = new Date(dateString);
+        return new Intl.DateTimeFormat('ca-ES', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric'
+        }).format(date);
+    }
 </script>
 
-<div class="mt-2 space-y-4">
-    {#each userNotes as note}
-        <UserNoteCard note={note} />
+<div class="mt-4">
+    {#if userNotes.length > 0}
+        <Table.Root>
+            <Table.Header>
+                <Table.Row class="hover:bg-transparent">
+                    <Table.Head>Títol</Table.Head>
+                    <Table.Head>Assignatura</Table.Head>
+                    <Table.Head class="hidden md:table-cell">Penjat</Table.Head>
+                    <Table.Head class="max-w-4"></Table.Head>
+                </Table.Row>
+            </Table.Header>
+            <Table.Body>
+                {#each userNotes as note}
+                    <Table.Row >
+                        <a href={`/usuari/apunts-penjats/${note.id}`} class="contents">
+                            <Table.Cell class="font-semibold">{note.title}</Table.Cell>
+                            <Table.Cell>{note.expand.subject.title}</Table.Cell>
+                            <Table.Cell class="hidden md:table-cell">{formatDate(note.created)}</Table.Cell>
+                            <Table.Cell class="max-w-4">
+                                <Edit class="w-4 h-4" />
+                            </Table.Cell>
+                        </a>
+                    </Table.Row>
+                {/each}
+            </Table.Body>
+        </Table.Root>
     {:else}
         <div class="text-center mt-16">
             <p class="mb-8 text-muted-foreground">Encara no has compartit cap apunt.</p>
             <Button href="/usuari/penja-apunts">
-                <Upload />
+                <Upload class="h-4 w-4 mr-2" />
                 Penja apunts
             </Button>
         </div>
-    {/each}
+    {/if}
 </div>
