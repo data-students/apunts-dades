98cc575261f36989ea974a30f5f1662e6bf91447
Adrià Blancafort <adriablancafort@gmail.com>
2025-03-02 00:12:21 +0100

    feat: add posthog

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
 	import '../app.css';
 	import { Toaster } from "$lib/components/ui/sonner/index.js";
+	import posthog from 'posthog-js'
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
 
 	let { children } = $props();
+
+	onMount(() => {
+		if (browser) {
+			posthog.init(
+				import.meta.env.VITE_PUBLIC_POSTHOG_KEY,
+				{ 
+					api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+					person_profiles: 'identified_only',
+				}
+			)
+		}
+		return
+	});
 </script>
 
 <Toaster theme="light" />
