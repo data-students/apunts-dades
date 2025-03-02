5a454f4fd556ded8ef4cdf140663156d5e4bf681
Adrià Blancafort <adriablancafort@gmail.com>
2025-03-02 13:05:52 +0100

    fix posthog config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	import { Toaster } from "$lib/components/ui/sonner/index.js";
-	import posthog from 'posthog-js'
-	import { browser } from '$app/environment';
-	import { onMount } from 'svelte';
 
 	let { children } = $props();
-
-	const posthogKey = import.meta.env.VITE_PUBLIC_POSTHOG_KEY;
-	const posthogHost = import.meta.env.VITE_PUBLIC_POSTHOG_HOST;
-
-	onMount(() => {
-		if (browser) {
-			posthog.init(
-				posthogKey,
-				{ 
-					api_host: posthogHost,
-					person_profiles: 'identified_only',
-				}
-			)
-		}
-		return
-	});
 </script>
 
 <Toaster theme="light" />
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,19 @@
+import posthog from 'posthog-js'
+import { browser } from '$app/environment';
+
+const posthogKey = import.meta.env.VITE_PUBLIC_POSTHOG_KEY;
+const posthogHost = import.meta.env.VITE_PUBLIC_POSTHOG_HOST;
+
+export const load = async () => {
+    if (browser) {
+        posthog.init(
+            posthogKey,
+            { api_host: posthogHost }
+        )
+    }
+    return
+};
+
 export const ssr = false;
 export const csr = true;
 export const prerender = false;
