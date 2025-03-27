45cea49f02e089aff7caa15249529459fc3a92ab
Adrià Blancafort <adriablancafort@gmail.com>
2025-03-27 17:26:21 +0100

    update termes i condicions link

--- src/routes/(unauthorised)/crea-compte/+page.svelte
+++ src/routes/(unauthorised)/crea-compte/+page.svelte
@@ -75,80 +75,90 @@
 </svelte:head>
 
 <div class="flex items-center justify-center h-screen">
-	<Card.Root class="max-w-sm mx-auto border-0 shadow-none sm:border sm:shadow-sm">
-		<Card.Header>
-			<Card.Title class="text-2xl">Crea un Compte</Card.Title>
-			<Card.Description
-				>Registra't a Apunts Dades per poder accedir i contribuir al repositori d'apunts del Grau en
-				Ciència i Enginyeria de Dades de la UPC.</Card.Description
-			>
-		</Card.Header>
-		<Card.Content>
-			<form class="grid gap-4" onsubmit={register}>
-				<div class="grid grid-cols-2 gap-4">
-					<div class="grid gap-2">
-						<Label for="first-name">Nom</Label>
-						<Input id="first-name" placeholder="Rosa" bind:value={firstName} required />
-					</div>
-					<div class="grid gap-2">
-						<Label for="last-name">Cognoms</Label>
-						<Input id="last-name" placeholder="Melano" bind:value={lastName} required />
-					</div>
-				</div>
-				<div class="grid gap-2">
-					<Label for="email">Correu electrònic</Label>
-					<Input
-						id="email"
-						type="email"
-						autocomplete="email"
-						placeholder="alumne@estudiantat.upc.edu"
-						bind:value={user.email}
-						required
-					/>
-					{#if !emailUPC}
-						<span class="text-sm text-red-500">Només s'accepten adreçes pertanyents a la UPC.</span>
-					{/if}
-				</div>
-				<div class="grid gap-2">
-					<Label for="password">Contrasenya</Label>
-					<Input id="password" type="password" required bind:value={user.password} />
-				</div>
-				<div class="grid gap-2">
-					<Label for="confirm-password">Confirma la Contrasenya</Label>
-					<Input id="confirm-password" type="password" required bind:value={user.passwordConfirm} />
-					{#if !passwordMatch}
-						<span class="text-sm text-red-500">Les contrasenyes han de coincidir.</span>
-					{/if}
-					{#if !passwordLength}
-						<span class="text-sm text-red-500">La contrasenya ha de tenir almenys 8 caracters.</span
-						>
-					{/if}
-				</div>
-				<Button
-					type="submit"
-					class="w-full"
-					disabled={!passwordMatch || !emailUPC || !passwordLength || loading}
+	<div>
+		<Card.Root class="max-w-sm mx-auto border-0 shadow-none sm:border sm:shadow-sm">
+			<Card.Header>
+				<Card.Title class="text-2xl">Crea un Compte</Card.Title>
+				<Card.Description
+					>Registra't a Apunts Dades per poder accedir i contribuir al repositori d'apunts del Grau
+					en Ciència i Enginyeria de Dades de la UPC.</Card.Description
 				>
-					{#if loading}
-						<LoaderCircle class="h-5 animate-spin" />
-					{:else}
-						Registra't
-					{/if}
+			</Card.Header>
+			<Card.Content>
+				<form class="grid gap-4" onsubmit={register}>
+					<div class="grid grid-cols-2 gap-4">
+						<div class="grid gap-2">
+							<Label for="first-name">Nom</Label>
+							<Input id="first-name" placeholder="Rosa" bind:value={firstName} required />
+						</div>
+						<div class="grid gap-2">
+							<Label for="last-name">Cognoms</Label>
+							<Input id="last-name" placeholder="Melano" bind:value={lastName} required />
+						</div>
+					</div>
+					<div class="grid gap-2">
+						<Label for="email">Correu electrònic</Label>
+						<Input
+							id="email"
+							type="email"
+							autocomplete="email"
+							placeholder="alumne@estudiantat.upc.edu"
+							bind:value={user.email}
+							required
+						/>
+						{#if !emailUPC}
+							<span class="text-sm text-red-500"
+								>Només s'accepten adreçes pertanyents a la UPC.</span
+							>
+						{/if}
+					</div>
+					<div class="grid gap-2">
+						<Label for="password">Contrasenya</Label>
+						<Input id="password" type="password" required bind:value={user.password} />
+					</div>
+					<div class="grid gap-2">
+						<Label for="confirm-password">Confirma la Contrasenya</Label>
+						<Input
+							id="confirm-password"
+							type="password"
+							required
+							bind:value={user.passwordConfirm}
+						/>
+						{#if !passwordMatch}
+							<span class="text-sm text-red-500">Les contrasenyes han de coincidir.</span>
+						{/if}
+						{#if !passwordLength}
+							<span class="text-sm text-red-500"
+								>La contrasenya ha de tenir almenys 8 caracters.</span
+							>
+						{/if}
+					</div>
+					<Button
+						type="submit"
+						class="w-full"
+						disabled={!passwordMatch || !emailUPC || !passwordLength || loading}
+					>
+						{#if loading}
+							<LoaderCircle class="h-5 animate-spin" />
+						{:else}
+							Registra't
+						{/if}
+					</Button>
+				</form>
+				<Separator class="my-5" />
+				<Button variant="outline" class="w-full space-x-1.5" onclick={registerWithGoogle}>
+					<img src="/icons/google.svg" alt="Google Logo" />
+					<span>Registra't amb Google</span>
 				</Button>
-			</form>
-			<Separator class="my-5" />
-			<Button variant="outline" class="w-full space-x-1.5" onclick={registerWithGoogle}>
-				<img src="/icons/google.svg" alt="Google Logo" />
-				<span>Registra't amb Google</span>
-			</Button>
-			<div class="mt-4 text-xs text-center">
-				Al registrar-te, acceptes els
-				<a href="/termes-i-condicions" class="underline">Termes i Condicions</a>
-			</div>
-			<div class="mt-6 text-sm text-center">
-				Ja tens compte?
-				<a href="/inicia-sessio" class="underline">Inicia sessió</a>
-			</div>
-		</Card.Content>
-	</Card.Root>
+				<div class="mt-4 text-sm text-center">
+					Ja tens compte?
+					<a href="/inicia-sessio" class="underline">Inicia sessió</a>
+				</div>
+			</Card.Content>
+		</Card.Root>
+		<div class="sm:mt-3 text-xs text-center text-muted-foreground">
+			Al registrar-te, acceptes els
+			<a href="/termes-i-condicions" class="underline">Termes i Condicions</a>
+		</div>
+	</div>
 </div>
